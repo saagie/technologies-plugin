740afa329c793938c746bc6d458c560c925034b2
fabien.mifsud <fabien.mifsud@saagie.com>
2021-09-30 15:48:26 +0200

    Disable incremental docker build on metadata modifications

--- src/main/kotlin/com/saagie/technologies/SaagieTechnologiesUtils.kt
+++ src/main/kotlin/com/saagie/technologies/SaagieTechnologiesUtils.kt
@@ -41,10 +41,17 @@ fun modifiedProjects(type: TYPE, subProjects: MutableSet<Project>): Set<Project>
         arrayOf(
             "bash",
             "-c",
-            "git diff --name-only  origin/master..."
+            "git diff --name-only origin/master"
         )
     ).inputStream.bufferedReader().readText()
         .split("\n").dropLast(1)
+        .filter { pathFile ->
+            !pathFile.contains("context.yaml", true) &&
+                !pathFile.contains("technology.yaml", true) &&
+                !pathFile.contains("innerContext.yaml", true) &&
+                !pathFile.contains("metadata.yaml", true) &&
+                !pathFile.contains("build.gradle.kts", true)
+        } // Do not rebuild docker images if metadata files are the only ones modified
         .forEach { pathFile ->
             if (pathFile.isA(type) && (File(pathFile).isFile || !File(pathFile).exists())) {
                 subProjects
